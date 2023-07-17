be0bc3df2a32d935419a4a31c0cd4cd38689a62b
Alamin Mirdha <mdalaminmirdha1211@gmail.com>
2023-07-17 21:47:14 +0600

    Update

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -1,59 +1,15 @@
 const Products = require('../models/uploadAllProductsModel');
 const csv = require('csvtojson');
 
-class APIfeatures{
-    constructor(query, queryString){
-        this.query = query;
-        this.queryString = queryString;
-    }
-    filtering(){
-        const queryObj = {...this.queryString} //queryString = req.query
-        // console.log({before: queryObj}) //before delete page
 
-        const excludedFields = ['page', 'sort', 'limit']
-        excludedFields.forEach(el => delete(queryObj[el]))
-
-        // console.log({after: queryObj}) //after delete page
-
-        let queryStr = JSON.stringify(queryObj)
-        queryStr = queryStr.replace(/\b(gte|gt|lt|lte|regex)\b/g, match=> '$' + match)
-        // console.log({ queryStr})
-
-        // gte = greater than or equal
-        // lte = lesser than or equal
-        // lt = lesser than
-        // gt = geater than 
-        // regex = for text search
-        this.query.find(JSON.parse(queryStr))
-
-        return this;
-    }
-
-    sorting(){
-        if(this.queryString.sort){
-            const sortBy = this.queryString.sort.split(',').join(' ')
-            this.query = this.query.sort(sortBy)
-        }else{
-            this.query = this.query.sort('-createdAt')
-        }
-        return this;
-    }
-
-    paginating(){
-        const page = this.queryString.page * 1 || 1
-        const limit = this.queryString.limit * 1 || 9
-        const skip = (page - 1) * limit;
-        this.query = this.query.skip(skip).limit(limit)
-        return this;
-    }
-}
 
 const productCtrl ={
 
     // This is get as MRP
     getAllProducts:async (req, res) => {
         try{
-            const allProducts = await Products.find();
+            const allProducts = await Products.find().limit(300);
+            console.log("hit");
             return res.json(allProducts);
         }catch(error){
             return res.send({status:400, success: false, msg:'Something went wrong'});
